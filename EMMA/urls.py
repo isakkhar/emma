d8d1767cdb5a5b1ddb850252587272c2d45e0738
Sakkhar Saha <isakkhar@gmail.com>
2025-02-28 20:40:05 +0600

    project url changed

--- EMMA/urls.py
+++ EMMA/urls.py
@@ -15,22 +15,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from mini import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('about', views.about, name='about'),
-    path('category', views.category, name='category'),
+    path('', include('mini.urls')),
 
-    path('portfolio', views.portfolio, name='portfolio'),
-    path('blog', views.blog, name='blog'),
-
-    path('contact', views.contact, name='contact'),
-
-    path('login', views.login, name='login'),
-
-    path('register', views.register, name='register'),
+    # path('', views.home, name='home'),
+    # path('about', views.about, name='about'),
+    # path('category', views.category, name='category'),
+    #
+    # path('portfolio', views.portfolio, name='portfolio'),
+    # path('blog', views.blog, name='blog'),
+    #
+    # path('contact', views.contact, name='contact'),
+    #
+    # path('login', views.login, name='login'),
+    #
+    # path('register', views.register, name='register'),
 ]
